4ca2baf9ed33db62325d75277416b58ceacb2cff
Albin Wrebo <albin.wrebo@outlook.com>
2025-05-28 09:22:59 +0200

    Configure Vercel deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,12 @@
 import app from './app';
 import config from './config/config';
 
-app.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`);
-});
+// For local development
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(config.port, () => {
+    console.log(`Server running on port ${config.port}`);
+  });
+}
 
+// For Vercel serverless functions
 export default app;
