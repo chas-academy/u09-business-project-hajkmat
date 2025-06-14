2a48429de2b106637291a9cabec4d420ff90e0f8
Albin Wrebo <78490851+beforeAW@users.noreply.github.com>
2025-06-14 15:51:10 +0200

    Update api.ts

--- frontend/hajkmat-frontend/src/config/api.ts
+++ frontend/hajkmat-frontend/src/config/api.ts
@@ -1,5 +1,5 @@
 const API_URL = import.meta.env.PROD
-  ? import.meta.env.VITE_API_URL || 'https://u09-business-project-hajkmat.onrender.com'
+  ? import.meta.env.VITE_API_URL || 'https://u09-business-project-hajkmat.onrender.com' + '/api'
   : '/api'; // In development
 
 export default API_URL;
