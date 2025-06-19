5ac2bb2a85b25d780091b68fb310c738754b3c3f
Albin Wrebo <78490851+beforeAW@users.noreply.github.com>
2025-06-19 10:04:29 +0200

    Update RecipelistMain.tsx

--- frontend/hajkmat-frontend/src/components/Main/Recipelist/RecipelistMain.tsx
+++ frontend/hajkmat-frontend/src/components/Main/Recipelist/RecipelistMain.tsx
@@ -2,7 +2,7 @@ import RecipeSearch from './Recipesearch';
 
 const RecipelistMain = () => {
   return (
-    <div className="container mx-auto px-4 flex justify-between items-center py-4 relative">
+    <div className="container mx-auto px-4 flex justify-between items-center py-4">
       <h1 className="text-3xl font-bold mb-6">Upptäck recept</h1>
       <p className="text-gray-600 mb-8">Leta upp recept för din hajk</p>
 
